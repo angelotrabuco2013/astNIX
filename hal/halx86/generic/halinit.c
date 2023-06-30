0abbb76f2050db95895ba4b1071790f3a04c8d86
Andrey Kudravets <>
2023-06-30 14:47:36 +0200

    [FREELDR][HAL] Full implementation of a scatter-gather functions.
    Improve check for EISA DMA.
    Add machine type detection (ISA / EISA) in the FREELDR.

--- hal/halx86/generic/halinit.c
+++ hal/halx86/generic/halinit.c
@@ -128,6 +128,10 @@ HalInitSystem(IN ULONG BootPhase,
         /* Setup I/O space */
         HalpDefaultIoSpace.Next = HalpAddressUsageList;
         HalpAddressUsageList = &HalpDefaultIoSpace;
+        if (HalpBusType == MACHINE_TYPE_EISA) {
+            HalpEisaIoSpace.Next = HalpAddressUsageList;
+            HalpAddressUsageList = &HalpEisaIoSpace;
+        }
 
         /* Setup busy waiting */
         HalpCalibrateStallExecution();
@@ -140,6 +144,8 @@ HalInitSystem(IN ULONG BootPhase,
          * so clear it here before interrupts are enabled
          */
         HalStopProfileInterrupt(ProfileTime);
+
+        HalpInitDma(LoaderBlock);
 
         /* Do some HAL-specific initialization */
         HalpInitPhase0(LoaderBlock);
